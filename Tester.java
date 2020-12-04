388a116774dd9fdf25d95b71040928160ad833f4
Yuqing Wu <ywu20@stuy.edu>
2020-12-04 13:28:18 -0500

    tests

--- Tester.java
+++ Tester.java
@@ -2,5 +2,11 @@ public class Tester{
   public static void main(String[] args){
     Range a=new Range(30,50);
     System.out.println(a);
+    System.out.println(a.length());
+    System.out.println(a.hasNext());
+    System.out.println(a.next());
+    System.out.println(a);
+    a.reset();
+    System.out.println(a);
   }
 }
